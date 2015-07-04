1b92ed1fc3c9b86eb8eeb205f5b9cf3f9357bcb1
tkmagesh <tkmagesh77@gmail.com>
2015-07-04 14:49:42 +0530

    Web Server - Part - 1

--- Day-01/03-PrintFile/program.js
+++ Day-01/03-PrintFile/program.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+/*
 fs.readFile("test.txt", {encoding : "utf8"}, function(err, contents){
     if (err){
         console.log("expected error", err);
@@ -6,3 +7,17 @@ fs.readFile("test.txt", {encoding : "utf8"}, function(err, contents){
     }
     console.log(contents);
 });
+*/
+
+var stream = fs.createReadStream("test.txt", {encoding : "utf8"});
+
+var readCount = 0;
+/*stream.on("data", function(chunk){
+    ++readCount;
+    console.log(chunk);
+});
+stream.on("end", function(){
+    console.log("EOF - with readCount : ", readCount);
+});*/
+
+stream.pipe(process.stdout);
