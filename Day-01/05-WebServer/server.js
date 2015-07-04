e22c397ea9d0eb600e5bb03a3fe606230cbffd72
tkmagesh <tkmagesh77@gmail.com>
2015-07-04 15:10:26 +0530

    Web Server - Part - 2

--- Day-01/05-WebServer/server.js
+++ Day-01/05-WebServer/server.js
@@ -1,9 +1,18 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
 
 var server = http.createServer(function(req, res){
-    console.log("a new connection is established for ", req.url);
-    res.write("<h1>Welcome to node.js</h1>");
-    res.end();
+    var resourcePath = path.join(__dirname, req.url);
+
+    console.log(resourcePath);
+    fs.createReadStream(resourcePath).pipe(res);
 });
+
 server.listen(8080);
 console.log("server listening on port 8080");
+
+/*
+res.statusCode = 404;
+
+*/
