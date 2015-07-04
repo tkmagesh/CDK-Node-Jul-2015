a441ef56ec8c22a8f188e2127f2d6c8183bf1143
tkmagesh <tkmagesh77@gmail.com>
2015-07-04 12:06:39 +0530

    Calculator - Part - 1

---
+++ Day-01/02-Intro/calculatorConsole.js
@@ -0,0 +1,17 @@
+var number1 = 100,
+    number2 = 200;
+var calculator = require("./calculator.js");
+console.log(calculator.add(number1, number2));
+console.log(calculator.subtract(number1, number2));
+console.log(calculator.multiply(number1, number2));
+console.log(calculator.divide(number1, number2));
+
+
+/*
+var calculator = require("./calculator");
+calculator.add(10);
+calculator.subtract(5);
+calculator.multiply(10);
+calculator.divide(5);
+calculator.getResult();
+*/
