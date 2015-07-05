60e8dd6b429f1c1589ab73d7e2255b08c7dc5d48
tkmagesh <tkmagesh77@gmail.com>
2015-07-05 16:37:57 +0530

    Network App - With EventEmitter

---
+++ Day-02/09-NetworkApp/Client/client.js
@@ -0,0 +1,18 @@
+var net = require("net");
+var MessageParser = require("./MessageParser");
+var changeCount = {
+
+}
+
+var socket = net.connect(8080);
+var parser = new MessageParser(socket);
+parser.on("watching", function(filename){
+    console.log("watching for file " + filename);
+});
+parser.on("change", function(filename){
+    changeCount[filename] = (changeCount[filename] || 0) + 1;
+    console.log(filename + " changed " + changeCount[filename] + " times");
+})
+parser.on("error", function(err){
+    console.log(err);
+});
